b0e436ed96ee4d0a81f2dcabc45ed412b17c2217
Eric Tanaka <cirept612@gmail.com>
2017-12-03 19:20:28 -0800

    adding tampermonkey script
    added tampermonkey script for users

---
+++ tampermonkey.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name Navigation Grabber - Local FILE ACCESS
+// @namespace www.cobaltgroup.com/
+// @author Eric Tanaka
+// @include http:*
+// @include https:*
+// @description Navigation Grabber
+// @require https://rawgit.com/cirept/NavigationGrabber/master/navigationGrabber.js
+// @run-at document-end
+// @grant GM_openInTab
+// @grant GM_setClipboard
+// @grant unsafeWindow
+// @grant GM_setValue
+// @grant GM_getValue
+// @grant GM_getResourceURL
+// @grant GM_info
+// @grant GM_listValues
+// @noframes
+// @grant GM_addStyle
+// @grant GM_getResourceText
+// ==/UserScript==
